c3a27a6887feafca03825c0d71d222c6b151935f
BettinaMubiligi <155474592+BettinaMubiligi@users.noreply.github.com>
2024-09-26 10:40:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,41 +1,54 @@
 from grille import *
 import numpy as np
 
-#grille=Grille("grille")
+grille=Grille("grille")
+
+
 #grille.place(4,(4,5),1)
-#grille.place(5,(2,3),2)
 #print(grille.grille)
 
+
+grille.place(5,(2,3),2)
+print(grille.grille)
+
 print("----------")
 
-#print(grille.place_alea(2))
+print(grille.place_alea(2))
 
 print("----------")
 
-""" 
-grille1=Grille("grille 1")
-grille1.place(4,(4,5),1)
-grille1.place(5,(2,3),2)
-print(grille1.grille)
-grille1.affiche() """
 
+#grille1=Grille("grille 1")
+
+
+#grille1.place(4,(4,5),1)
+#grille1.place(5,(2,3),2)
+
+#print(grille1.grille)
 #print("----------")
 
 #print(grille.eq(grille1))
 print("----------")
 
+#print(Grille.genere_grille().grille)
 
+#grille.affiche()
 
 grille2 = Grille.genere_grille("grille 2")
-print(grille2.grille)
-grille2.affiche() 
+#print(grille2.grille)
 
+grille2.affiche()
 
+#print(peut_placer(grille,5,(5,8),1))
+#print(peut_placer(grille,5,(2,3),1))
 
 #grille1 = place(grille,4,(4,5),1)
 #grille = place(grille,3,(3,8),2)
+print("------------------")
+print(nb_placements_possibles_bateau(5))
 
-
+print("----------------")
+print (nb_placements_possibles_liste_sans_chev([1,4,5]))
 
 #print(place(grille,4,(4,5),1))
 #print("----------------")
